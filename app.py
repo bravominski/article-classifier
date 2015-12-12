b0568cf1a6774f02cb666a20820116587b1b01a1
bravominski <Minski@hou195.wireless-pennnet.upenn.edu>
2015-12-12 17:54:04 -0500

    i

--- app.py
+++ app.py
@@ -2,42 +2,22 @@ from flask import Flask, render_template, request
 from flask.ext.heroku import Heroku
 
 from nltk.corpus import stopwords
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.feature_extraction import DictVectorizer
 try:
    import cPickle as pickle
 except:
    import pickle
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/articleclassification'
-# db = SQLAlchemy(app)
 heroku = Heroku(app)
 clf = pickle.load(open("article_clf.pkl","rb"))
 dv = pickle.load(open("article_dv.pkl","rb"))
 stop = stopwords.words('english')
-
-# Create our database model
-# class Article(db.Model):
-#     __tablename__ = "articles"
-#     id = db.Column(db.Integer, primary_key=True)
-#     text = db.Column(db.Text, unique=True)
-#     label = db.Column(db.String(0))
-
-#     def __init__(self, text, label):
-#         self.text = text
-#         self.label = label
-
-#     def __repr__(self):
-#         return '<Text %r>' % self.text
 
 # Set "homepage" to index.html
 @app.route('/')
 def index(): 
     return render_template('index.html')
 
-# Save e-mail to database and send to success page
 @app.route('/label', methods=['POST'])
 def label():
     article = None
